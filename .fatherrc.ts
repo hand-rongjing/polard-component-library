f57918d98336a00a1440aa1e0a3b6cb020c67922
王纵 <zong.wang01@hand-china.com>
2022-05-09 18:06:29 +0800

    conflict: 测试冲突提交

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,3 +1,12 @@
+/*
+ * @Author: zong.wang01@hand-china.com
+ * @Date: 2021-12-27 11:54:12
+ * @LastEditors: zong.wang01@hand-china.com
+ * @LastEditTime: 2022-05-09 18:05:59
+ * @Version: 1.0.0
+ * @Description:
+ * @Copyright: Copyright (c) 2021, Hand-RongJing
+ */
 // import nodePolyfills from 'rollup-plugin-node-polyfills';
 // import commonjs from 'rollup-plugin-commonjs';
 
@@ -5,6 +14,7 @@ export default {
   esm: 'rollup',
   cjs: 'rollup',
   extractCSS: true,
+
   // umd: {
   //   name: 'hand-polard',
   //   sourcemap: true,
